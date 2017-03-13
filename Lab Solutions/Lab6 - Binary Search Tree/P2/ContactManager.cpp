587bfc024566b64adbb0cabe4f4d18ae81f62068
Edda Steinunn <eddast15@ru.is>
2017-03-13 11:51:05 +0000

    Added comments

--- Lab Solutions/Lab6 - Binary Search Tree/P2/ContactManager.cpp
+++ Lab Solutions/Lab6 - Binary Search Tree/P2/ContactManager.cpp
@@ -1,12 +1,16 @@
 #include "ContactManager.h"
 
+// Adds contact if name or phone are not duplicates
+// Otherwise an exception is thrown
 void ContactManager::add(Contact contact) {
 	if(name_exists(contact.name)){ throw DuplicateNameException(); }
-    if(phone_exists(contact.phone)){ throw DuplicatePhoneException(); }
-    name_map.add(contact.name, contact);
+	if(phone_exists(contact.phone)){ throw DuplicatePhoneException(); }
+	name_map.add(contact.name, contact);
 	phone_map.add(contact.phone, contact);
 }
 
+// Removes contact from both phone_map and name_map by name
+// No action if name is not found in list
 void ContactManager::remove(string name) {
 	if(name_map.contains(name)){
         string phonenr = (name_map.isFound(name)->value).phone;
@@ -15,6 +19,10 @@ void ContactManager::remove(string name) {
     }
 }
 
+// Phone number updated by name in both phone_map and name_map
+// Key changed in phone_map, so needs to be removed from the map, then added again
+// with other values unchanged
+// if name not found an exception is thrown
 void ContactManager::update_phone(string name, string new_number) {
     if(name_exists(name)){
         Contact cvalue = name_map.isFound(name)->value;
@@ -29,6 +37,8 @@ void ContactManager::update_phone(string name, string new_number) {
     }
 }
 
+// Email updated in both maps, no key change needed
+// Exception thrown if name matches no contact
 void ContactManager::update_email(string name, string new_email) {
     if(name_exists(name)){
         Contact cvalue = name_map.isFound(name)->value;
@@ -42,18 +52,22 @@ void ContactManager::update_email(string name, string new_email) {
     }
 }
 
+// Returns true if name exists in map
 bool ContactManager::name_exists(string name) {
     return name_map.isFound(name);
 }
 
+// Returns true if phone exists in map
 bool ContactManager::phone_exists(string phone) {
 	return phone_map.isFound(phone);
 }
 
+// Gets contact by name and returns it's contact value
 Contact ContactManager::get_contact_by_name(string name) {
 	return (name_map.isFound(name))->value;
 }
 
+// Gets contact by phone and returns it's contact value
 Contact ContactManager::get_contact_by_phone(string phone) {
 	return (phone_map.isFound(phone))->value;
 }
